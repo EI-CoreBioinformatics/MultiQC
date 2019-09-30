30e7fe9ac50f737e2b9ef8e829b074ca2143d07d
Christian Schudoma <cschu1981@gmail.com>
2019-09-30 16:02:50 +0100

    Hotfix/20190930 bcl2fastq undetermined barcode issue (#1)
    bcl2fastq module now parses undetermined barcodes using Lane:<lane_id> value instead of indexing into the UnknownBarcodes via assumed lane index.

--- multiqc/modules/bcl2fastq/bcl2fastq.py
+++ multiqc/modules/bcl2fastq/bcl2fastq.py
@@ -123,7 +123,7 @@ class MultiqcModule(BaseMultiqcModule):
         # Add section with undetermined barcodes
         self.add_section(
             name = "Undetermined barcodes by lane",
-            anchor = "undetermine_by_lane",
+            anchor = "undetermined_by_lane",
             description = "Count of the top twenty most abundant undetermined barcodes by lanes",
             plot = bargraph.plot(
                 self.get_bar_data_from_undetermined(self.bcl2fastq_bylane),
@@ -167,13 +167,12 @@ class MultiqcModule(BaseMultiqcModule):
             rlane = run_data[lane]
 
             # Add undetermined barcodes
-            try:
-                unknown_barcode = content['UnknownBarcodes'][l - 1]['Barcodes']
-            except IndexError:
-                unknown_barcode = next(
-                    (item['Barcodes'] for item in content['UnknownBarcodes'] if item['Lane'] == 8),
-                    None
-                )
+            for lane_data in content.get("UnknownBarcodes", list()):
+                if lane_data["Lane"] == l:
+                    unknown_barcode = lane_data["Barcodes"]
+                    break
+            else:
+                unknown_barcode = dict()
             run_data[lane]['unknown_barcodes'] = unknown_barcode
 
             for demuxResult in conversionResult.get("DemuxResults", []):
